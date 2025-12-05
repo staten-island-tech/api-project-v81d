c3110e71ae626cfc8bc944245a4593e22fd70f60
v81d <v81d@proton.me>
2025-12-04 22:32:25 -0500

    feat(frontend): create basic frontend boilerplate

---
+++ face-classifier/src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div class="flex flex-col justify-center items-center w-full h-full">
+    The main content will go here.
+  </div>
+`;
