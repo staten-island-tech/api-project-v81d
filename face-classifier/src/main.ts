834715233c970689f20d3bb135c2bc6580489e70
v81d <v81d@proton.me>
2025-12-05 10:35:55 -0500

    feat(frontend): add camera to main page
    refactor: add type annotations

--- face-classifier/src/main.ts
+++ face-classifier/src/main.ts
@@ -1,7 +1,21 @@
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="flex flex-col justify-center items-center w-full h-full">
-    The main content will go here.
-  </div>
-`;
+const app: HTMLDivElement = document.querySelector("#app")!;
+const formContainer: HTMLDivElement = document.createElement("div");
+
+formContainer.className = "form-container";
+app.appendChild(formContainer);
+
+navigator.mediaDevices
+  .getUserMedia({ video: true })
+  .then((stream: MediaStream) => {
+    const video = document.createElement("video");
+
+    video.srcObject = stream;
+    video.autoplay = true;
+
+    formContainer.appendChild(video);
+  })
+  .catch((e: Error) => {
+    console.error("An error occurred displaying the camera:", e);
+  });
--- face-classifier/vite.config.ts
+++ face-classifier/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), mkcert()],
 });
